1a0585762393854e810614fed65506c5ddffa455
Mariana Fernandez <marfer9292@gmail.com>
2024-07-13 14:51:10 -0300

    Se modifico

--- registro.js
+++ registro.js
@@ -1,5 +1,4 @@
 function enviarDatos() {
-  // Obtener los datos del formulario
   var email = document.getElementById("email").value;
   var contrasenia = document.getElementById("contrasenia").value;
   var nombre = document.getElementById("nombre").value;
@@ -8,7 +7,6 @@ function enviarDatos() {
   var provincia = document.getElementById("provincia").value;
   var postal = document.getElementById("postal").value;
 
-  // Crear objeto con los datos para enviar en el body
   var datos = {
     email: email,
     contra: contrasenia,
@@ -19,12 +17,10 @@ function enviarDatos() {
     codigoPostal: postal,
   };
 
-  // Convertir el objeto a JSON
   var json = JSON.stringify(datos);
 
   var url = "http://localhost:8081/usuario/crear";
 
-  // Ejecutar la llamada a la API REST
   fetch(url, {
     method: "POST",
     headers: {
@@ -35,10 +31,8 @@ function enviarDatos() {
     .then((response) => response.json())
     .then((data) => {
       console.log("Respuesta de la API:", data);
-      // Aquí puedes manejar la respuesta de la API según tus necesidades
     })
     .catch((error) => {
       console.error("Error al enviar datos:", error);
-      // Aquí puedes manejar errores en la llamada a la API
     });
 }
